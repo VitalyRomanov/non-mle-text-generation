e4fbcfb646a0a72be02f4016bb8e0f8a544e04e2
Albina Khusainova <asayfeeva@gmail.com>
2021-04-14 18:44:36 +0300

    fixing gpu

--- split_train_val_test.py
+++ split_train_val_test.py
@@ -1,6 +1,7 @@
 import logging
 import os
 import random
+import sys
 from typing import Tuple, Iterable
 
 from transformers import T5Tokenizer
@@ -206,11 +207,44 @@ def generate_nmt_splits(
     print("bad sentences:", bad_counter)
 
 
+def generate_nmt_splits2(
+        src_lang, tgt_lang, dataset_path, output_path, tokenizer=None, prefix=""
+):
+
+    def generate_partition(partition):
+        src_sents = open(os.path.join(dataset_path, f"{partition}.{src_lang}")).readlines()
+        tgt_sents = open(os.path.join(dataset_path, f"{partition}.{tgt_lang}")).readlines()
+        assert len(src_sents) == len(tgt_sents)
+
+        for src_sent, tgt_sent in zip(src_sents, tgt_sents):
+            yield prefix+src_sent, tgt_sent
+
+    if not os.path.isdir(output_path):
+        os.mkdir(output_path)
+
+    write_splits(
+        output_path,
+        generate_partition("train"),
+        generate_partition("valid"),
+        generate_partition("test"),
+        src_lang, tgt_lang, tokenizer
+    )
+
+
 if __name__ == "__main__":
     src_lang = 'cs'
     tgt_lang = 'en'
 
     file_dir = 'data-bin/'
+
+    generate_nmt_splits2(
+        src_lang="en",
+        tgt_lang="ro",
+        dataset_path="wmt16_en-ro",
+        output_path=file_dir + src_lang + '-' + tgt_lang,
+        tokenizer="t5-small",
+        prefix="translate English to Romanian: "
+    )
 
     # # TODO check out dataset binarization
     # generate_nmt_splits(
@@ -226,69 +260,69 @@ if __name__ == "__main__":
     #     tokenizer="t5-small"
     # )
 
-    src_file_n = open(file_dir + src_lang + '-' + tgt_lang + '/tokenized_full.' + src_lang, 'r')
-    tgt_file_n = open(file_dir + src_lang + '-' + tgt_lang + '/tokenized_full.' + tgt_lang, 'r')
-
-    src_lines = src_file_n.readlines()
-    tgt_lines = tgt_file_n.readlines()
-
-    src_file_n.close()
-    tgt_file_n.close()
-
-    src_file_train = open(file_dir + src_lang + '-' + tgt_lang + '/train.' + src_lang, 'w')
-    tgt_file_train = open(file_dir + src_lang + '-' + tgt_lang + '/train.' + tgt_lang, 'w')
-
-    src_file_test = open(file_dir + src_lang + '-' + tgt_lang + '/test.' + src_lang, 'w')
-    tgt_file_test = open(file_dir + src_lang + '-' + tgt_lang + '/test.' + tgt_lang, 'w')
-
-    src_file_val = open(file_dir + src_lang + '-' + tgt_lang + '/valid.' + src_lang, 'w')
-    tgt_file_val = open(file_dir + src_lang + '-' + tgt_lang + '/valid.' + tgt_lang, 'w')
-
-    # limit = 802000
-    assert len(src_lines) == len(tgt_lines)
-
-    max_sent_len = 1000
-    test_sents = 0
-    val_sents = 0
-    train_sents = 0
-    test_val_size = 6000
-    train_size = 80000
-    bad_counter = 0
-    for i in range(len(src_lines)):
-        # if limit == 0:
-        #     break
-        src = src_lines[i]
-        tgt = tgt_lines[i]
-        # check for duplicates and empty lines and filter by length
-        if src != tgt and (max_sent_len > len(src.strip()) != 0) and (max_sent_len > len(tgt.strip()) != 0):
-            # limit -= 1
-            if test_sents < test_val_size:
-                src_file_test.write(src)
-                tgt_file_test.write(tgt)
-                test_sents += 1
-            elif val_sents < test_val_size:
-                src_file_val.write(src)
-                tgt_file_val.write(tgt)
-                val_sents += 1
-            else:
-                src_file_train.write(src)
-                tgt_file_train.write(tgt)
-                train_sents += 1
-                if train_sents == train_size:
-                    break
-        else:
-            print(src, tgt)
-            bad_counter += 1
-
-    print("bad sentences:", bad_counter)
-
-    src_file_train.close()
-    tgt_file_train.close()
-
-    src_file_test.close()
-    tgt_file_test.close()
-
-    src_file_val.close()
-    tgt_file_val.close()
+    # src_file_n = open(file_dir + src_lang + '-' + tgt_lang + '/tokenized_full.' + src_lang, 'r')
+    # tgt_file_n = open(file_dir + src_lang + '-' + tgt_lang + '/tokenized_full.' + tgt_lang, 'r')
+    #
+    # src_lines = src_file_n.readlines()
+    # tgt_lines = tgt_file_n.readlines()
+    #
+    # src_file_n.close()
+    # tgt_file_n.close()
+    #
+    # src_file_train = open(file_dir + src_lang + '-' + tgt_lang + '/train.' + src_lang, 'w')
+    # tgt_file_train = open(file_dir + src_lang + '-' + tgt_lang + '/train.' + tgt_lang, 'w')
+    #
+    # src_file_test = open(file_dir + src_lang + '-' + tgt_lang + '/test.' + src_lang, 'w')
+    # tgt_file_test = open(file_dir + src_lang + '-' + tgt_lang + '/test.' + tgt_lang, 'w')
+    #
+    # src_file_val = open(file_dir + src_lang + '-' + tgt_lang + '/valid.' + src_lang, 'w')
+    # tgt_file_val = open(file_dir + src_lang + '-' + tgt_lang + '/valid.' + tgt_lang, 'w')
+    #
+    # # limit = 802000
+    # assert len(src_lines) == len(tgt_lines)
+    #
+    # max_sent_len = 1000
+    # test_sents = 0
+    # val_sents = 0
+    # train_sents = 0
+    # test_val_size = 6000
+    # train_size = 80000
+    # bad_counter = 0
+    # for i in range(len(src_lines)):
+    #     # if limit == 0:
+    #     #     break
+    #     src = src_lines[i]
+    #     tgt = tgt_lines[i]
+    #     # check for duplicates and empty lines and filter by length
+    #     if src != tgt and (max_sent_len > len(src.strip()) != 0) and (max_sent_len > len(tgt.strip()) != 0):
+    #         # limit -= 1
+    #         if test_sents < test_val_size:
+    #             src_file_test.write(src)
+    #             tgt_file_test.write(tgt)
+    #             test_sents += 1
+    #         elif val_sents < test_val_size:
+    #             src_file_val.write(src)
+    #             tgt_file_val.write(tgt)
+    #             val_sents += 1
+    #         else:
+    #             src_file_train.write(src)
+    #             tgt_file_train.write(tgt)
+    #             train_sents += 1
+    #             if train_sents == train_size:
+    #                 break
+    #     else:
+    #         print(src, tgt)
+    #         bad_counter += 1
+    #
+    # print("bad sentences:", bad_counter)
+    #
+    # src_file_train.close()
+    # tgt_file_train.close()
+    #
+    # src_file_test.close()
+    # tgt_file_test.close()
+    #
+    # src_file_val.close()
+    # tgt_file_val.close()
 
     # procedure for files - clean and split to 3 parts - 6k test, 6k validation, remainder - train
