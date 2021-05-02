0f9ada886f5480bfc83fd1b543a82e9c20dc29e4
Vitaly R <mortiv16@gmail.com>
2021-05-02 17:17:31 +0300

    added positional encodings

--- discriminator.py
+++ discriminator.py
@@ -1,3 +1,5 @@
+import math
+
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
@@ -110,6 +112,7 @@ class AttDiscriminator(nn.Module):
         # self.decoder = nn.TransformerDecoder(self.decoder_layer, num_layers=layers)
         self.encoder_layer = nn.TransformerEncoderLayer(emb_dim, num_heads, dim_feedforward=emb_dim)
         self.encoder = nn.TransformerEncoder(self.encoder_layer, num_layers=layers)
+        self.pos_encoder = PositionalEncoding(emb_dim, dropout)
 
         self.target_mask = self.generate_square_subsequent_mask(1)
         # self.memory_mask = self.generate_square_subsequent_mask(1)
@@ -129,6 +132,7 @@ class AttDiscriminator(nn.Module):
         # if self.memory_mask.size(0) != source_emb.size(0):
         #     self.memory_mask = self.generate_square_subsequent_mask(source_emb.size(0)).to(source_ids.device)
         # out = self.decoder(target_emb, source_emb, tgt_mask=self.target_mask, memory_mask=self.target_mask)
+        target_emb = self.pos_encoder(target_emb)
         out = self.encoder(target_emb, mask=self.target_mask)
 
         out = self.fc(out)
@@ -140,6 +144,24 @@ class AttDiscriminator(nn.Module):
         mask = mask.float().masked_fill(mask == 0, float('-inf')).masked_fill(mask == 1, float(0.0))
         return mask
 
+
+class PositionalEncoding(nn.Module):
+
+    def __init__(self, d_model, dropout=0.1, max_len=5000):
+        super(PositionalEncoding, self).__init__()
+        self.dropout = nn.Dropout(p=dropout)
+
+        pe = torch.zeros(max_len, d_model)
+        position = torch.arange(0, max_len, dtype=torch.float).unsqueeze(1)
+        div_term = torch.exp(torch.arange(0, d_model, 2).float() * (-math.log(10000.0) / d_model))
+        pe[:, 0::2] = torch.sin(position * div_term)
+        pe[:, 1::2] = torch.cos(position * div_term)
+        pe = pe.unsqueeze(0).transpose(0, 1)
+        self.register_buffer('pe', pe)
+
+    def forward(self, x):
+        x = x + self.pe[:x.size(0), :]
+        return self.dropout(x)
 
 class GumbelDiscriminator(nn.Module):
     def __init__(self, args, src_dict, dst_dict, emb_dim=50, use_cuda=True, dropout=0.1, num_heads=1, layers=1):
